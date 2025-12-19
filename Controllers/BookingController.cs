a356974d96a73d13cf7d320ab1b7234621596d20
Noor  Fatima <noorefatimah0@gmail.com>
2025-12-19 11:49:18 +0500

    BookingController

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Karigar.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
