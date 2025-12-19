f1dd0840be5b0b0aa2409788912fe2d66db65932
Noor  Fatima <noorefatimah0@gmail.com>
2025-12-19 11:32:47 +0500

    Updated ServicesProviderController

--- Controllers/ServiceProviderController.cs
+++ Controllers/ServiceProviderController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Karigar.Models;
+using Karigar.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
 
 namespace Karigar.Controllers
 {
+    [Authorize(Roles = "ServiceProvider")]
     public class ServiceProviderController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public ServiceProviderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Dashboard()
         {
             return View();
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ServiceProviderProfile model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            model.UserId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier).Value;
+
+            _context.ServiceProviderProfiles.Add(model);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Dashboard");
+        }
+
     }
+
 }
