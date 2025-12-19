066f1bf99db45fd10366f3926d3b35dc2b485bb0
Noor  Fatima <noorefatimah0@gmail.com>
2025-12-19 11:40:05 +0500

    Define Review class with properties

--- Models/Review.cs
+++ Models/Review.cs
@@ -1,6 +1,7 @@
-﻿namespace Karigar.Models
-{
-    public class Review
-    {
-    }
+public class Review {
+    public int Id { get; set; }
+    public int ProviderId { get; set; }
+    public string CustomerName { get; set; }
+    public int Rating { get; set; } // 1-5
+    public string Comment { get; set; }
 }
